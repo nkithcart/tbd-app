52cb265b31ccdec018154ba771d6454aa116e859
Noah Kithcart <noah.kithcart@parivedasolutions.com>
2017-06-23 14:10:46 -0400

    UPDATED Project Configuration: -- ejected webpack in order to customize in future [NOTE: this changes run commands, see package.json for more info] -- added vendor.ts to better organize project dependencies
    FIXED issue with AWS sdk not being included in project build:
    -- moved placement of library listing in package.config -> seemed to be downloading some libraries, but not 'aws-sdk'
    -- added import statement to vendor file
    -- modified tsconfig entries to ensure library will be bundled with others

---
+++ src/vendor.ts
@@ -0,0 +1,16 @@
+// import entire SDK 
+import * as AWS from 'aws-sdk';
+
+// // Angular
+// import '@angular/platform-browser';
+// import '@angular/platform-browser-dynamic';
+// import '@angular/core';
+// import '@angular/common';
+// import '@angular/http';
+// import '@angular/router';
+
+// RxJS
+import 'rxjs';
+
+// Other vendors for example jQuery, Lodash or Bootstrap
+// You can import js, ts, css, sass, ...
